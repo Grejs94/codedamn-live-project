b2987fabe0938d64d3ece3213022addb729a53e5
unknown <gracjanludyga@gmail.com>
2021-01-11 16:02:35 +0100

    start to add MongoDB

--- server/build/server.js
+++ server/build/server.js
@@ -4,9 +4,22 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-// cconst express = require('express') //js
+const body_parser_1 = __importDefault(require("body-parser"));
+// we can get json payload from the request
+const cors_1 = __importDefault(require("cors"));
+// enable console error with duble localhost 3000 and 1337 (cors policy)
 const app = express_1.default();
+if (process.env.NODE_ENV !== "production") {
+    app.use(cors_1.default());
+}
+// only in dev mode we need this because when we deploy to AWS we will have only one server
+app.use(body_parser_1.default.json());
+// enable us to use body from requests from the client site
 app.get("/", (req, res) => {
     res.send("ok");
+});
+app.post("/api/register", (req, res) => {
+    console.log(req.body);
+    res.json({ status: "ok" });
 });
 app.listen(1337);
--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,27 @@
 import express from "express";
-
-// cconst express = require('express') //js
+import bodyParser from "body-parser";
+// we can get json payload from the request
+import cors from "cors";
+// enable console error with duble localhost 3000 and 1337 (cors policy)
 
 const app = express();
 
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors());
+}
+// only in dev mode we need this because when we deploy to AWS we will have only one server
+
+app.use(bodyParser.json());
+// enable us to use body from requests from the client site
+
 app.get("/", (req: any, res: any) => {
   res.send("ok");
+});
+
+app.post("/api/register", (req, res) => {
+  console.log(req.body);
+
+  res.json({ status: "ok" });
 });
 
 app.listen(1337);
